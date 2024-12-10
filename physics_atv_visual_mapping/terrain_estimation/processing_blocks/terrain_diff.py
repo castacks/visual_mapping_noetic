217b7e3628ef6be8562cec8a55510e61251f00bb
Sam <striest@andrew.cmu.edu>
2024-12-10 16:37:36 -0500

    and overhang param to terrain_diff block

--- physics_atv_visual_mapping/terrain_estimation/processing_blocks/terrain_diff.py
+++ physics_atv_visual_mapping/terrain_estimation/processing_blocks/terrain_diff.py
@@ -8,11 +8,12 @@ class TerrainDiff(TerrainEstimationBlock):
     """
     Compute a per-cell min and max height
     """
-    def __init__(self, voxel_metadata, voxel_n_features, terrain_layer, max_elevation_layer, max_elevation_mask_layer, device):
+    def __init__(self, voxel_metadata, voxel_n_features, terrain_layer, max_elevation_layer, max_elevation_mask_layer, overhang, device):
         super().__init__(voxel_metadata, voxel_n_features, device)
         self.terrain_layer = terrain_layer
         self.max_elevation_layer = max_elevation_layer
         self.max_elevation_mask_layer = max_elevation_mask_layer
+        self.overhang = overhang
 
     def to(self, device):
         self.device = device
@@ -33,7 +34,7 @@ class TerrainDiff(TerrainEstimationBlock):
         max_elevation_mask = bev_grid.data[..., max_elevation_mask_idx] > 1e-4
 
         #divide by resolution to get slope as m/m instead of m/cell
-        diff = max_elevation_data - terrain_data
+        diff = (max_elevation_data - terrain_data).clip(0., self.overhang)
         diff[~max_elevation_mask] = 0.
 
         diff_idx = bev_grid.feature_keys.index(self.output_keys[0])
