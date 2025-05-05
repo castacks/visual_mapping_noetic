08fd852f279345d372311b4d37a494e49510683d
striest <striest@andrew.cmu.edu>
2025-05-05 13:55:52 -0700

    have overhang calc only check voxels in range (#10)

--- physics_atv_visual_mapping/terrain_estimation/processing_blocks/terrain_diff.py
+++ physics_atv_visual_mapping/terrain_estimation/processing_blocks/terrain_diff.py
@@ -8,11 +8,9 @@ class TerrainDiff(TerrainEstimationBlock):
     """
     Compute a per-cell min and max height
     """
-    def __init__(self, voxel_metadata, voxel_n_features, terrain_layer, max_elevation_layer, max_elevation_mask_layer, overhang, device):
+    def __init__(self, voxel_metadata, voxel_n_features, terrain_layer, overhang, device):
         super().__init__(voxel_metadata, voxel_n_features, device)
         self.terrain_layer = terrain_layer
-        self.max_elevation_layer = max_elevation_layer
-        self.max_elevation_mask_layer = max_elevation_mask_layer
         self.overhang = overhang
 
     def to(self, device):
@@ -26,16 +24,24 @@ class TerrainDiff(TerrainEstimationBlock):
     def run(self, voxel_grid, bev_grid):
         terrain_idx = bev_grid.feature_keys.index(self.terrain_layer)
         terrain_data = bev_grid.data[..., terrain_idx].clone()
-        
-        max_elevation_idx = bev_grid.feature_keys.index(self.max_elevation_layer)
-        max_elevation_data = bev_grid.data[..., max_elevation_idx].clone()
 
-        max_elevation_mask_idx = bev_grid.feature_keys.index(self.max_elevation_mask_layer)
-        max_elevation_mask = bev_grid.data[..., max_elevation_mask_idx] > 1e-4
+        voxel_grid_idxs = voxel_grid.raster_indices_to_grid_indices(voxel_grid.feature_raster_indices)
+        voxel_grid_pts = voxel_grid.grid_indices_to_pts(voxel_grid_idxs, centers=True)
 
-        #divide by resolution to get slope as m/m instead of m/cell
-        diff = (max_elevation_data - terrain_data).clip(0., self.overhang)
-        diff[~max_elevation_mask] = 0.
+        voxel_terrain_height = terrain_data[voxel_grid_idxs[:, 0], voxel_grid_idxs[:, 1]]
+        voxel_hdiff = voxel_grid_pts[:, 2] - voxel_terrain_height
+        voxel_valid_mask = voxel_hdiff < self.overhang
+
+        #bev grid idxs are the first 2 dims of the voxel idxs assuming matching metadata
+        raster_idxs = voxel_grid_idxs[:, 0] * bev_grid.metadata.N[1] + voxel_grid_idxs[:, 1]
+
+        idxs_to_scatter = raster_idxs[voxel_valid_mask]
+        features_to_scatter = voxel_hdiff[voxel_valid_mask]
+    
+        num_cells = (bev_grid.metadata.N[0] * bev_grid.metadata.N[1]).item()
+
+        diff = torch_scatter.scatter(src=features_to_scatter, index=idxs_to_scatter, dim_size=num_cells, reduce='max')
+        diff = diff.view(*bev_grid.metadata.N).clip(0., self.overhang)
 
         diff_idx = bev_grid.feature_keys.index(self.output_keys[0])
         bev_grid.data[..., diff_idx] = diff
